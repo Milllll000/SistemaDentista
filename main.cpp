df088652c6d2079a53e84691c8e89222365e88b0
Lewdominity <124177609+Lewdominity@users.noreply.github.com>
2024-06-08 09:37:46 -0600

    Crear archivo main.cpp con la función main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Función main para el sistema de citas
+ * 
+ * Autor: Allan Mauricio Brenes Castro
+ * 
+ * Fecha de creación: 07/06/2024
+ * Fecha de última modificación: 08/06/2024
+*/
+
+int main(int argc, char const *argv[])
+{
+    
+}
